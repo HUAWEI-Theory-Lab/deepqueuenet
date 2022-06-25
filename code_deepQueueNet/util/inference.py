a64b3208b30b95b4a15171ad673db83e6c5644b2
kerryyang <30408130+YangQingQingKerry@users.noreply.github.com>
2022-06-25 18:28:00 +0800

    gpu-deployment

--- code_deepQueueNet/util/inference.py
+++ code_deepQueueNet/util/inference.py
@@ -8,14 +8,6 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # Apache-2.0 License for more details.
 
-"""
-This script is to implement the inference on fattree16.   
-     1. model_input       traffic -> feature extraction
-     2. infer             deepQueueNet
-     3. link_info_upd       
-     4. trace_upd         device-level inference   
-     5. run               topology-level inference in parallel  
-"""
 import tensorflow as tf 
 import os
 import pandas as pd 
@@ -27,12 +19,28 @@ from code_deepQueueNet.tools.error_corr import  get_error_dis, error_correction
 import warnings
 warnings.filterwarnings("ignore")
 
- 
+
+
+
+"""
+This script is to implement the inference on fattree16.   
+INDEXs - PCs: from left to right are labeled from 0 to 15.
+       - Switchs:
+         1. core-layer: from left to right, are labeled from 0 to 3
+         2. aggregation-layer: from left to right, are labeled from 4 to 11
+         3. edge-layer: from left to right, are labeled from 12 to 19
+       - PORT NUMBERs of a switch: from bottom to top, left to right, are labeled 0,1,2 and 3 respectively.
+FUNCTIONS:
+     1. model_input       traffic -> feature extraction
+     2. infer             deepQueueNet
+     3. link_info_upd       
+     4. trace_upd         device-level inference   
+     5. run               topology-level inference in parallel  
+"""
 
 
 
 
-    
 class INFER(Init_TOPO):
     def __init__(self, filename, config, model_config):
         self.filename=filename
@@ -154,7 +162,7 @@ class INFER(Init_TOPO):
  
         
                 
-    def run(self):
+    def run(self, gpu_number=1):
         tf.logging.set_verbosity(tf.logging.ERROR)
     
         self.used_cols=['index', 
@@ -172,35 +180,55 @@ class INFER(Init_TOPO):
         
         
      
-        def build_and_clean(device, my_result):
-            for i in range(4*device, 4*(device+1)): my_result[i]=pd.DataFrame(columns=self.used_cols)
+       
         with Manager() as MG:
             my_result=MG.dict() 
             my_link=MG.dict(LINKS)
             
-            #single gpu deployment
+            
+            
+            #deploy the task on multi-GPUs. Each GPU excutes the inference on a sub-graph.
             def pod_infer(device, my_result, my_link):
                 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-                os.environ["CUDA_VISIBLE_DEVICES"] = '{}'.format(device)   
+                os.environ["CUDA_VISIBLE_DEVICES"] = '{}'.format(device) 
+                if gpu_number==1:
+                    core_bg, core_ed= 0,4
+                    agg_bg, agg_ed= 4,12
+                    edge_bg, edge_ed=12,20 
+                    pc_bg, pc_ed=0,16 
+                elif gpu_number==2:
+                    core_bg, core_ed=((0,2), (2,4))[device]
+                    agg_bg, agg_ed=((4,8), (8,12))[device]
+                    edge_bg, edge_ed=((12,16), (16,20))[device]
+                    pc_bg, pc_ed=((0,8), (8,16))[device]
+                elif gpu_number==4:
+                    core_bg, core_ed=((0,1), (1,2), (2,3), (3,4))[device]
+                    agg_bg, agg_ed=((4,6), (6,8), (8,10), (10,12))[device]
+                    edge_bg, edge_ed=((12,14), (14,16), (16,18), (18,20))[device]
+                    pc_bg, pc_ed=((0,4), (4,8), (8,12), (12,16))[device]
+                 
+                 
+                 
+                
                 with tf.Session() as sess:
                     saver = tf.train.import_meta_graph('{}/model.ckpt-{}.meta'.format(self.model_config.model, self.model_config.md))
                     saver.restore(sess, '{}/model.ckpt-{}'.format(self.model_config.model, self.model_config.md)) 
                     X = tf.get_collection('X')[0]   
                     outputs=tf.get_collection('outputs')[0]
                     #IRSA
-                    build_and_clean(device, my_result)
-                    for i in range(12+2*device, 14+2*device): self.trace_upd('edge', i, sess, outputs, X, my_result, my_link)
-                    build_and_clean(device, my_result)
-                    for i in range(4+2*device, 6+2*device): self.trace_upd('agg', i, sess, outputs, X, my_result, my_link)
-                    self.trace_upd('core', device, sess, outputs, X,my_result, my_link) 
-                    for i in range(4+2*device, 6+2*device): self.trace_upd('agg', i, sess, outputs, X, my_result, my_link)
-                    for i in range(12+2*device, 14+2*device): self.trace_upd('edge', i, sess, outputs, X, my_result, my_link)
+                    for i in range(pc_bg, pc_ed): my_result[i]=pd.DataFrame(columns=self.used_cols)
+                    for i in range(edge_bg, edge_ed): self.trace_upd('edge', i, sess, outputs, X, my_result, my_link)
+                    for i in range(pc_bg, pc_ed): my_result[i]=pd.DataFrame(columns=self.used_cols) #clear the unstable results
+                    for i in range(agg_bg, agg_ed): self.trace_upd('agg', i, sess, outputs, X, my_result, my_link)
+                    for i in range(core_bg, core_ed): self.trace_upd('core', i, sess, outputs, X,my_result, my_link) 
+                    for i in range(agg_bg, agg_ed): self.trace_upd('agg', i, sess, outputs, X, my_result, my_link)
+                    for i in range(edge_bg, edge_ed): self.trace_upd('edge', i, sess, outputs, X, my_result, my_link)
 
                             
             #parallel reasoning                
             t0=time.time()         
             threads = []
-            for i in range(4): 
+            for i in range(gpu_number): 
                 t=Process(target=pod_infer, args=(i, my_result, my_link))
                 threads.append(t)
                 t.start()
